943700bb5a95b6eaee3bf7eadc0fdb80a81de6ef
Diogo Santos <diogosantosdasilva364@gmail.com>
2023-06-05 23:25:32 -0300

    Aulas
    Na aula3 nos métodos em instâncias de Classes Python.
    Na aula4 entendemos o que  self em class Python.

---
+++ aula4.py
@@ -0,0 +1,25 @@
+# Entendendo self em classes Python
+# Classe - Molde (geralmete sem dados)
+# Instãncia da class (objeto) Tem os dados
+# Uma classe pode gerar varis instâncias
+# Na classe o self é a propria instância.
+
+class Carro:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def acelerar(self):
+        print(f'{self.nome} está acelerando...')
+
+
+fusca = Carro('Fusca')
+fusca.acelerar()
+Carro.acelerar(fusca)
+# print(fusca.nome)
+# fusca.acelerar()
+
+celta = Carro(nome='Celta')
+celta.acelerar()
+Carro.acelerar(celta)
+# print(celta.nome)
+# celta.acelerar()
